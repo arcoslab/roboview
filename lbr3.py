074d6704aef47a81bb2ea4f3dad1829fb6a108f1
Ingo Kresse <kresse@cs.tum.edu>
2009-02-16 16:55:37 +0100

    changed LBR3 description to better reflect real world.

--- lbr3.py
+++ lbr3.py
@@ -1,24 +1,43 @@
 from PyKDL import *
 from math import pi
 
+#segments = [
+#   Segment(Joint(Joint.None),
+#     Frame(Rotation(),Vector(0,0,0.110))),
+#   Segment(Joint(Joint.RotZ),
+#     Frame(Rotation.RotX(-pi/2),Vector(0,0,0.200))),
+#   Segment(Joint(Joint.RotZ),
+#     Frame(Rotation.RotX(pi/2),Vector(0,-0.200,0))),
+#   Segment(Joint(Joint.RotZ),
+#     Frame(Rotation.RotX(-pi/2),Vector(0,0.0,0.200))),
+#   Segment(Joint(Joint.RotZ,-1),
+#     Frame(Rotation.RotX(pi/2),Vector(0,-0.200,0))),
+#    Segment(Joint(Joint.RotZ),
+#     Frame(Rotation.RotX(-pi/2),Vector(0,0,0.190))),
+#    Segment(Joint(Joint.RotZ),
+#     Frame(Rotation.RotX(pi/2),Vector(0,-0.078,0))),
+#    Segment(Joint(Joint.RotZ),
+#     Frame(Vector(0,0,0.100))),
+#     ]
+
 segments = [
-   Segment(Joint(Joint.None),
-     Frame(Rotation(),Vector(0,0,0.110))),
-   Segment(Joint(Joint.RotZ),
-     Frame(Rotation.RotX(-pi/2),Vector(0,0,0.200))),
-   Segment(Joint(Joint.RotZ),
-     Frame(Rotation.RotX(pi/2),Vector(0,-0.200,0))),
-   Segment(Joint(Joint.RotZ),
-     Frame(Rotation.RotX(-pi/2),Vector(0,0.0,0.200))),
-   Segment(Joint(Joint.RotZ,-1),
-     Frame(Rotation.RotX(pi/2),Vector(0,-0.200,0))),
-    Segment(Joint(Joint.RotZ),
-     Frame(Rotation.RotX(-pi/2),Vector(0,0,0.190))),
-    Segment(Joint(Joint.RotZ),
-     Frame(Rotation.RotX(pi/2),Vector(0,-0.078,0))),
-    Segment(Joint(Joint.RotZ),
-     Frame(Vector(0,0,0.100))),
-     ]
+  Segment(Joint(Joint.None),
+    Frame(Rotation.Identity(), Vector(0.0, 0.0, 0.11))),
+  Segment(Joint(Joint.RotZ),
+    Frame(Rotation.RotX(-pi/2), Vector(0.0, 0.0, 0.20))),
+  Segment(Joint(Joint.RotZ),
+    Frame(Rotation.RotX(pi/2), Vector(0.0, -0.20, 0.0))),
+  Segment(Joint(Joint.RotZ),
+    Frame(Rotation.RotX(pi/2), Vector(0, 0, .20))),
+  Segment(Joint(Joint.RotZ),
+    Frame(Rotation.RotX(-pi/2), Vector(0, 0.2, 0))),
+  Segment(Joint(Joint.RotZ),
+    Frame(Rotation.RotX(-pi/2), Vector(0, 0, 0.19))),
+  Segment(Joint(Joint.RotZ),
+    Frame(Rotation.RotX(pi/2), Vector(0, -0.078, 0.0))),
+  Segment(Joint(Joint.RotZ),
+    Frame(Rotation.Identity(), Vector(0, 0.0, 0.0)))]
+
 
 limits_min = [-169.9, -119.9, -169.9, -119.9, -169.9, -119.9, -169.9]
 limits_max = [ 169.9,  119.9,  169.9,  119.9,  169.9,  119.9,  169.9]
